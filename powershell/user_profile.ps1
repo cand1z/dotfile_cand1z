fe1418563cb989a64fc4d3bad8791d10979a58c3
cand1z <daviaraujocandido@gmail.com>
2023-04-19 02:11:29 -0300

    task(powershell) - adding powershell config files

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,24 @@
+# Prompt
+oh-my-posh init pwsh --config 'C:\Users\Davi\.config\powershell\cand1z.omp.json' | Invoke-Expression
+
+# Terminal-Icons
+Import-Module Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
