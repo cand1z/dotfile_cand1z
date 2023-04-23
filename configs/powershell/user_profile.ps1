1d4cabea000df61919042acb6914622ac53b7d6a
cand1z <daviaraujocandido@gmail.com>
2023-04-22 22:00:13 -0300

    mod(user_profile.ps1): removing line never used

--- configs/powershell/user_profile.ps1
+++ configs/powershell/user_profile.ps1
@@ -7,7 +7,6 @@ Import-Module Terminal-Icons
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
